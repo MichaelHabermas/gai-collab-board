2b1f6a459e64dcf3c47fabde34ce2317714dab5c
habermoose <michaelghabermas@gmail.com>
2026-02-16 21:06:07 -0500

    feat(sync): export new services from sync module
    - Export cursor and presence functions from realtimeService
    - Export object CRUD functions from objectService
    - Export ICursorData and IPresenceData types

--- src/modules/sync/index.ts
+++ src/modules/sync/index.ts
@@ -1,3 +1,4 @@
+// Board service exports
 export {
   createBoard,
   getBoard,
@@ -13,3 +14,33 @@ export {
   canUserManage,
   type ICreateBoardParams,
 } from "./boardService";
+
+// Realtime service exports (cursors, presence, connection)
+export {
+  updateCursor,
+  subscribeToCursors,
+  removeCursor,
+  setupCursorDisconnectHandler,
+  updatePresence,
+  subscribeToPresence,
+  removePresence,
+  setupPresenceDisconnectHandler,
+  subscribeToConnectionStatus,
+  getUserColor,
+  type ICursorData,
+  type IPresenceData,
+} from "./realtimeService";
+
+// Object service exports
+export {
+  createObject,
+  createObjectsBatch,
+  updateObject,
+  updateObjectsBatch,
+  deleteObject,
+  deleteObjectsBatch,
+  subscribeToObjects,
+  mergeObjectUpdates,
+  type ICreateObjectParams,
+  type IUpdateObjectParams,
+} from "./objectService";
